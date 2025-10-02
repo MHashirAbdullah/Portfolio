779f182703700ed563f55d28f62376f5ad533be3
Hashir Abdullah <hashirabdullah46@gmail.com>
2025-10-02 11:32:36 +0500

    create hero section left side

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+import Hero from "./sections/Hero";
+
 const App = () => {
   return (
-    <main>
-      <div>
-        <h1 className="text-5xl">Portfolio</h1>
-      </div>
-    </main>
+    <>
+    <Hero/>
+    </>
   );
 };
 
 export default App;
-
---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const words = [
+  { text: "Ideas", imgPath: "/public/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/public/images/concepts.svg" },
+  { text: "Designs", imgPath: "/public/images/designs.svg" },
+  { text: "Code", imgPath: "/public/images/code.svg" },
+  { text: "Ideas", imgPath: "/public/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/public/images/concepts.svg" },
+  { text: "Design", imgPath: "/public/images/designs.svg" },
+  { text: "Code", imgPath: "/public/images/code.svg" },
+];
