ebafdaae71d53be752e542037a928a6c542018d2
Hashir Abdullah <hashirabdullah46@gmail.com>
2025-10-04 23:33:22 +0500

    add hero section enhancements with animated counter and button component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import { words } from "../constants/index.js";
+import Button from "../components/Button.jsx";
+import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import AnimatedCounter from "../components/AnimatedCounter.jsx";
 const Hero = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".hero-text h1",
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, stagger: 0.2, duration: 1, ease: "power2.inOut" }
+    );
+  });
   return (
     <section id="hero" className="relative overflow-hidden">
       <div className="absolute top-0 left-0 z-10">
@@ -8,7 +20,7 @@ const Hero = () => {
       </div>
 
       <div className="hero-layout">
-        {/* Left side */}
+        {/* LEFT: Hero Content */}
         <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
           <div className="flex flex-col gap-7">
             <div className="hero-text">
@@ -16,13 +28,17 @@ const Hero = () => {
                 Shaping
                 <span className="slide">
                   <span className="wrapper">
-                    {words.map((word)=>(
-                      <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2">
-                        <img src={word.imgPath} alt={word.text}
-                        className="xl:size-12 md:size 10 size-7 md:p-2 p-1 rounded-full bg-white-50" />
-                        <span>
-                          {word.text}
-                        </span>
+                    {words.map((word, index) => (
+                      <span
+                        key={index}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt="person"
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word.text}</span>
                       </span>
                     ))}
                   </span>
@@ -31,10 +47,29 @@ const Hero = () => {
               <h1>into Real Projects</h1>
               <h1>that Deliver Results</h1>
             </div>
+
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+              Hi, I'm Hashir Abdullah, a Software Engineer Based in Pakistan{" "}
+              <br />
+              with a Passion for Turning Ideas into Reality.
+            </p>
+
+            <Button
+              text="See My Work"
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="counter"
+            />
           </div>
         </header>
-        {/* Right side */}
+
+        {/* RIGHT: 3D Model or Visual */}
+        <figure>
+          <div className="hero-3d-layout">
+            <HeroExperience />
+          </div>
+        </figure>
       </div>
+      <AnimatedCounter/>
     </section>
   );
 };
